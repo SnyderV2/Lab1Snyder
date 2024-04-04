9953d13ff076797a4e3c91b5feb7e6f3514f8a46
Kaiden <ksnyder33@rockets.utoledo.edu>
2024-04-04 14:46:26 -0400

    first commit

---
+++ Lab1Snyder/Laboratory.cpp
@@ -0,0 +1,26 @@
+/*
+ * Program:  <First Laboratory>
+ * Author:   <Kaiden Snuder
+ * Date:     <January 25, 2024>
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+
+	cout << "Rectangle Area = " << area << "\n";
+	cout << "Who's on first? \n.";
+
+	return 0;
+}
